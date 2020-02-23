d9efe911eec1c18f405921aa2ea284a50914a362
Chris <BitPoet@users.noreply.github.com>
2020-02-23 15:10:24 +0100

    Assign current user in hasVoted if not passed
    Make sure that hasVoted() falls back to the currently logged in user if it is called from renderPoll and use_user is set.

--- Pollino.module.php
+++ Pollino.module.php
@@ -419,6 +419,7 @@ class Pollino extends WireData implements Module {
         }
 
         if($this->prevent_voting_type == "use_user"){
+            if($user === null) $user = $this->wire('user');
             return $this->checkByUser($poll, $user);
         }
 
